ed338138a55ae4a67fe70962d45e47fc34f670f0
gsun <gsun@scu.edu>
2018-05-04 19:48:27 -0700

    p3.cpp

--- critical.h
+++ critical.h
@@ -1,9 +1,19 @@
 #ifndef CRITICAL_H
 #define CRITICAL_H
 
-
+//inputs
 int input_customer;
 
+//number of sellers
+int h_seller = 1;
+int m_seller = 3;
+int l_seller = 6;
+
+//total available seats left
+int remaining_seats = 0;
+
+//mutex and cond
+pthread_mutex_t mutex = PTHREAD_COND_INITIALIZER;
+pthread_cond_t cond = PTHREAD_MUTEX_INITIALIZER;
 
 #endif
-
