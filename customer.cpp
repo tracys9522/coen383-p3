1abb52d77a9b1e4c6901a89a43b5750c18d63678
Erik Trewitt <etrewitt@users.noreply.github.com>
2018-05-09 11:43:30 -0700

    added seat id field

--- customer.cpp
+++ customer.cpp
@@ -4,11 +4,21 @@ customer::customer()
 {
     _custid = -1;
     _arrival = -1;
+    _seatid = -1;
 }
 customer::customer(int custid, int arrival)
 {
     _custid = custid;
     _arrival = arrival;
+    _seatid = -1;
+}
+
+void customer::set_id(int id) {
+  _custid = id;
+}
+
+void customer::set_seat(int id) {
+  _seatid = id;
 }
 
 int customer::custid() const
@@ -19,6 +29,10 @@ int customer::custid() const
 int customer::arrival() const
 {
     return _arrival;
+}
+
+int customer::seatid() const {
+  return _seatid;
 }
 
 customer& customer::operator=(const customer& c){
@@ -32,4 +46,3 @@ ostream &operator << (ostream &ostr, const customer cust)
     ostr<<"cust id: "<<cust.custid() << " arrival: "<< cust.arrival();
     return ostr;
 }
-
--- customer.h
+++ customer.h
@@ -7,11 +7,15 @@ using namespace std;
 class customer{
     int _arrival;
     int _custid;
+    int _seatid;
 public:
     customer();
     customer(int custid, int arrival);
+    void set_id(int id);
+    void set_seat(int id);
     int arrival() const;
     int custid() const;
+    int seatid() const;
     customer& operator=(const customer& c);
 };
 ostream &operator <<(ostream &ostr, const customer cust);
